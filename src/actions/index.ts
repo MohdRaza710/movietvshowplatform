88b1b171eb57942000a59f6c596126ac3ea51abf
MohdRaza710 <mrmraza.710@gmail.com>
2026-05-30 14:43:53 +0300

    chnages

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,7 +1,7 @@
 'use server'
 
 import { createServerClient } from '@/lib/supabase-server'
-import { Review, WatchlistItem } from '@/types/database'
+import { Review, WatchlistItem } from '@/types'
 import { revalidatePath } from 'next/cache'
 
 // Auth Actions
